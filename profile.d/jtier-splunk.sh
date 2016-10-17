a63e1444eda599421428a5182d1ce6ad67eb2e7d
Kyle Fritz <kyle.p.fritz@gmail.com>
2016-10-17 15:46:17 -0400

    start splunk forwarder?

--- profile.d/jtier-splunk.sh
+++ profile.d/jtier-splunk.sh
@@ -1,1 +1,6 @@
-echo "lets start that splunk agent!!"
+cd ~/vendor/splunkforwarder/bin/
+./splunk start --accept-license
+touch ~/$SPLUNK_LOG_FILE
+./splunk add forward-server $SPLUNK_INDEXER -auth admin:changeme
+./splunk add monitor ~/$SPLUNK_LOG_FILE  -auth admin:changeme
+echo "goapi_jtier_splunk_agent_started" >> ~/splunk-log
--- readme.md
+++ readme.md
@@ -3,3 +3,8 @@
 1. Make jTier required directories
 1. Install & Configure Splunk Agent
 1. Install & Configure Java Metrics
+
+## Environment Variables
+
+    $ flynn env set SPLUNK_LOG_FILE=splunk-log 
+    $ flynn env set SPLUNK_INDEXER=splunk-staging-indexers.snc1:9997
